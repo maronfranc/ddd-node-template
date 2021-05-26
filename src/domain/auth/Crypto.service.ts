814f91ccd6d6e261750692ccfaede43525192d83
Máron <maron.franca@gmail.com>
2021-05-26 00:46:28 -0300

    add crypto service

---
+++ src/domain/auth/Crypto.service.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcrypt';
+import { IUser } from '../../infrastructure/mongo/user';
+
+export class CryptoService {
+  private SALT_ROUNDS = 10;
+  public async hash(unhashedPassword: string, salt: string): Promise<string> {
+    return bcrypt.hash(unhashedPassword, salt);
+  }
+  public async genSalt(saltRounds = this.SALT_ROUNDS): Promise<string> {
+    return bcrypt.genSalt(saltRounds);
+  }
+  public async compareUserPasswords(
+    unhashedPassword: string,
+    user: Pick<IUser, 'salt' | 'password'>
+  ): Promise<boolean> {
+    if (!user.password || !user.salt) throw new Error('Internal server error');
+    const cryptoService = new CryptoService();
+    const password = await cryptoService.hash(unhashedPassword, user.salt);
+    return password === user.password;
+  }
+}
