d475b51c3010401fdc3ece13fc3819eb24cd98c0
Máron <maron.franca@gmail.com>
2021-05-25 23:07:28 -0300

    add infrastructure in main

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import 'reflect-metadata'
 import { Application } from './application/Application';
+import { Infrastructure } from './infrastructure';
 
 function main() {
   const PORT = 4001;
+  const infrastructure = new Infrastructure();
+  infrastructure.init({
+    onConnected: () => {
+      console.info('================================================');
+      console.info('Database connected');
+      console.info('================================================');
+    },
+    onError: (error) => {
+      console.info('================================================');
+      console.info('Database connection error', error);
+      console.info('================================================');
+      const WITH_ERROR = 1;
+      process.exit(WITH_ERROR);
+    }
+  });
   const application = new Application();
-  application.routes();
+  application.init();
   application.listen(PORT, () => {
-    console.log(`Server is running at https://localhost:${PORT}`);
+    console.info('================================================');
+    console.info(`Server is running at https://localhost:${PORT}`);
+    console.info('================================================');
   });
 }
 main();
