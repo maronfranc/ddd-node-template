37f473f4fa3e7df02bfe9ad0d6cfb4b9c4457817
Máron França <maron.franca@gmail.com>
2024-03-29 16:42:39 -0300

    [refactor] remove unused code.

--- api/src/application/controller/todo-list/todo-list.websocket.ts
+++ api/src/application/controller/todo-list/todo-list.websocket.ts
@@ -1,5 +1,3 @@
-import Infrastructure from "../../../infrastructure/Infrastructure";
-import todoListRepository from "../../../infrastructure/mongo/todo-list/todo-list.repository";
 import { Get } from "../../library/decorators";
 import { Param, WsConnection } from "../../library/decorators/route-params";
 import { WebSocketGateway } from "../../library/decorators/websocket.decorator";
@@ -12,37 +10,11 @@ export class TodoListWebsocket {
     @Param('id') id: string,
     @WsConnection() conn: WebSocket,
   ) {
-    const todoListRepository = Infrastructure.repositories.todoList;
-    const changeStream = todoListRepository.watch(id);
-    // console.log(`[Log(${typeof changeStream}):changeStream]:`, changeStream);
-
-    changeStream.on('change', (change) => {
-      console.log(`[Log(${typeof change}):change]:`, change);
-      conn.send(JSON.stringify({
-        message: "change detected",
-        data: change,
-      }))
-    });
-    // changeStream.
-    // for await (let change of changeStream) {
-    //   console.log("Received change:\n", change);
-    // }
-    // await changeStream.close();
-
+    console.log(id);
     conn.on('message', async (message) => {
-      conn.send('23812897129')
+      console.info(`Message received: ${message}`)
+      conn.send('Message received.')
     })
-
-
-    // for (let i = 0; i <= 5; i++) {
-    //   await sleep(1000)
-    //   conn.send(JSON.stringify({ message: "Base message:" + i }))
-    // }
     return { ok: true }
   }
-}
-function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
 }
